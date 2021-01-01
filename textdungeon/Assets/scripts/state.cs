61c14dc689eaf8bf927b5ebbb2806e230859237c
Ruthvik <ruthvik.2123@gmail.com>
2021-01-01 07:16:18 -0500

    basic dungeon engine

---
+++ textdungeon/Assets/scripts/state.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "State")]
+public class state : ScriptableObject
+{
+    [TextArea(14,10)][SerializeField] string stateText;
+    [SerializeField] state[] stateArray;
+
+    public string getStateText()
+    {
+        return stateText;
+    }
+
+    public state[] getStateArray()
+    {
+        return stateArray;
+    }
+}
---
+++ textdungeon/Assets/scripts/textBehaviourScript.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class textBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Text textfield;
+    [SerializeField] state startingstate;
+
+
+    state currentState;
+    state[] stateArray;
+    void Start()
+    {
+        currentState = startingstate;
+        textfield.text = currentState.getStateText();
+        stateArray = currentState.getStateArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        manageStates();
+    }
+
+    private void manageStates()
+    {
+        stateArray = currentState.getStateArray();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentState = stateArray[0];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentState = stateArray[1];
+        }
+
+        textfield.text = currentState.getStateText();
+    }
+}
