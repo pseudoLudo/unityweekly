d47182c27f858bd0681858609d983b946bd678c7
Ruthvik <ruthvik.2123@gmail.com>
2021-01-05 07:22:15 -0500

    number wizard

---
+++ numwizUI/Assets/gameplay.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class gameplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int mid, guess;
+    [SerializeField] int min = 0, max = 1000;
+    [SerializeField] TextMeshProUGUI guessText;
+    void Start()
+    {
+        starter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //resultcheck();
+    }
+
+    void starter()
+    {
+        min = 0; max = 1000;
+        guess = Random.Range(min, max);
+        mid = Random.Range(min, max);
+        max++;
+        guessText.text = mid.ToString();
+    }
+
+    public void onHigh()
+    {
+        min = mid;
+        midsetter();
+    }
+    public void onLow()
+    {
+        max = mid;
+        midsetter();
+    }
+    void midsetter()
+    {
+        mid = Random.Range(min, max);
+        Debug.Log("is it higher or lower than:" + mid);
+        guessText.text = mid.ToString();
+    }
+
+   public  void resultcheck()
+    {
+        if (guess == mid)
+        {
+            Debug.Log("you reached it");
+            Start();
+        }
+        else if (mid == min || mid == max)
+        {
+            Debug.Log("did not reach it");
+            Start();
+        }
+    }
+}
---
+++ numwizUI/Assets/sceneLoad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneLoad : MonoBehaviour
+{
+    public void loadScene()
+    {
+        int getSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (getSceneIndex == 2)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(getSceneIndex + 1);
+        }
+        
+    }
+}
